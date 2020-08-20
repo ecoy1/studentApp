ebea52f525be6a6fae0818c1d5933fd7ebf0c945
Nikol Stoeva <nikolstst00@gmail.com>
2020-08-20 17:58:16 +0300

    Colours

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,23 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
-//hiiiiiiiii
+void main() => runApp(MaterialApp(
+  home: Home(),
+));
 
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromRGBO( 125, 49, 100 , 1),
+        title: Text('This be burgundy'),
+      ),
+      backgroundColor: Colors.grey[900],
+    );
+  }
+}
